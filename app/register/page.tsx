7c7ccb31ffd008d77802e77a67ad595a6575f910
shayanabedi98 <shayanabedi@hotmail.com>
2024-09-10 02:08:44 -0400

    no substanstial changes, just tryna keep this up to date with main

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,7 +4,27 @@ export default function Register() {
   return (
     <div className="ancestor-container">
       <Container>
-        <div></div>
+        <div className="flex items-center justify-center">
+          <div className="flex w-1/2 flex-col justify-center gap-8">
+            <h1>Become a Vendor</h1>
+            <h2>
+              Make your stone collection visible to users as a{" "}
+              <strong>Vendor</strong>. Gain access to our services for a fee of
+              $53.99/month. You handle your inventory, we&apos;ll handle the
+              marketing.
+            </h2>
+            <p>As a vendor you can:</p>
+            <ul className="list-disc pl-8">
+              <li>Make unlimited posts of your stone slabs</li>
+              <li>Import images of your stones</li>
+              <li>Set prices for each stone slab</li>
+              <li>Manage and update your inventory easily</li>
+              <li>Receive direct customer inquiries through our platform</li>
+              <li>Cancel your subscription at any time</li>
+            </ul>
+          </div>
+          <div className="w-1/2"></div>
+        </div>
       </Container>
     </div>
   );
